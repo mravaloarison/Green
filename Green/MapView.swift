42263f92721b50b82d5b0bf67ed2e86be4c4eb8e
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-09 13:40:25 -0500

    MapView Added with default coordination

---
+++ Green/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Green
+//
+//  Created by Mami Ravaloarison on 11/9/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
