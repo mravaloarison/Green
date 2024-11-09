1ba6256e14ab26f27b118b1dafaf95893121b10d
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-09 17:05:32 -0500

    location manager + search autocomplete

---
+++ Green/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Green
+//
+//  Created by Mami Ravaloarison on 11/9/24.
+//
+
+import Foundation
---
+++ Green/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  Green
+//
+//  Created by Mami Ravaloarison on 11/9/24.
+//
+
+import Foundation
