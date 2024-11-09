3ba692002cf9a44a869af892b9cbe4897a289816
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-09 13:13:05 -0500

    Initial Commit

---
+++ Green/GreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenApp.swift
+//  Green
+//
+//  Created by Mami Ravaloarison on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
