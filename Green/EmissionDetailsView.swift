6ae79c20e784d496c4de9b48c08d1cad4e65e83e
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-11-10 02:52:47 -0500

    Emisiion details view updated

--- Green/EmissionDetailsView.swift
+++ Green/EmissionDetailsView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct EmissionDetailsView: View {
+    @State private var isCarbonFootprintActive: Bool = true
+    
     var body: some View {
-        Text("Emission details view")
+        VStack {
+            Toggle(isOn: $isCarbonFootprintActive, label: {
+                HStack (spacing: 12) {
+                    Image(systemName: "carbon.dioxide.cloud.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(isCarbonFootprintActive ? .green : .secondary)
+                        .frame(width: 36, height: 36)
+                    Text("Save the earth")
+                        .font(.title3)
+                }
+            })
+            .toggleStyle(SwitchToggleStyle(tint: .green))
+            Spacer()
+        }
+        .padding()
     }
 }
 
